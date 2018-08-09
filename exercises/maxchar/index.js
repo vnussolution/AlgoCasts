731568d2d64630e0d70eac78492210c77f3ef2d3
VN-US Solution <vnussolution@gmail.com>
2018-08-09 16:53:10 -0700

    solve Max Char in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    let obj = {};
+    let returnValue = ''
+    let max = 0;
+
+    for (let c of str) {
+        obj[c] = obj[c] + 1 || 1;
+    }
+    for (const o in obj) {
+
+        if (obj[o] > max) {
+            returnValue = o;
+            max = obj[o];
+        }
+
+    }
+    console.log(str, obj, returnValue);
+    return returnValue;
+
+}
 
 module.exports = maxChar;
